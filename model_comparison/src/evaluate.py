6171ea0ac3d59b4332c58c561e08d65bb275773b
heetbhuva1801 <heetbhuva18@gmail.com>
2025-02-24 12:36:25 +0530

    Removed previously uploaded separate files

--- model_comparison/src/evaluate.py
+++
@@ -1,6 +0,0 @@
-from sklearn.metrics import silhouette_score
-
-def evaluate_clustering(X, labels):
-    score = silhouette_score(X, labels)
-    print(f"Silhouette Score: {score}")
-    return score
